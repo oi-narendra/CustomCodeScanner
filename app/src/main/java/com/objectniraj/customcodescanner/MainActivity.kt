b20781c79a71aeeee711595695c9e9ac32568430
Narendra <nirajbhatt1997@gmail.com>
2020-04-29 10:59:36 +0545

    added torch support and scanner

--- app/src/main/java/com/objectniraj/customcodescanner/MainActivity.kt
+++ app/src/main/java/com/objectniraj/customcodescanner/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.objectniraj.customcodescanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startScan.setOnClickListener {
+            IntentIntegrator(this)
+                .setCaptureActivity(ScannerActivity::class.java)
+                .setBeepEnabled(true)
+                .setOrientationLocked(true)
+                .setBarcodeImageEnabled(true)
+                .setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+                .setCameraId(0)
+                .initiateScan()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        //check for null
+        if (result != null) {
+            if (result.contents != null) {
+
+                //Scanned Result
+                scannedCodeResult.text = result.contents
+
+                //Scanned Code Image
+                Glide.with(applicationContext)
+                    .load(result.barcodeImagePath)
+                    .optionalCenterCrop()
+                    .into(scannedCodeImage)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
---
+++ app/src/main/java/com/objectniraj/customcodescanner/ScannerActivity.kt
@@ -0,0 +1,74 @@
+package com.objectniraj.customcodescanner
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.journeyapps.barcodescanner.CaptureManager
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
+import kotlinx.android.synthetic.main.content_scanner.*
+
+class ScannerActivity : AppCompatActivity(), DecoratedBarcodeView.TorchListener {
+
+
+    private var isFlashOn = false
+    private var capture: CaptureManager? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_scanner)
+
+        this.overridePendingTransition(
+            android.R.anim.fade_in,
+            android.R.anim.fade_out
+        )
+
+        barcodeScanner.setTorchListener(this)
+
+        flash.setOnClickListener {
+            isFlashOn = if (isFlashOn) {
+                barcodeScanner.setTorchOff()
+                false
+            } else {
+                barcodeScanner.setTorchOn()
+                true
+            }
+        }
+        capture = CaptureManager(this, barcodeScanner)
+        capture!!.initializeFromIntent(intent, savedInstanceState)
+        capture!!.decode()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        capture!!.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        capture!!.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        capture!!.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        capture!!.onSaveInstanceState(outState)
+    }
+
+    override fun onTorchOn() {
+        flash.setImageResource(R.drawable.ic_flash_on)
+    }
+
+    override fun onTorchOff() {
+        flash.setImageResource(R.drawable.ic_flash_off)
+    }
+
+}
